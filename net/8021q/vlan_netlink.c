755c0860fef374b199814a1c2ac895c43fe4baf3
Xin Long <lucien.xin@gmail.com>
2022-02-09 03:19:55 -0500

    vlan: introduce vlan_dev_free_egress_priority
    commit 37aa50c539bcbcc01767e515bd170787fcfc0f33 upstream.
    
    This patch is to introduce vlan_dev_free_egress_priority() to
    free egress priority for vlan dev, and keep vlan_dev_uninit()
    static as .ndo_uninit. It makes the code more clear and safer
    when adding new code in vlan_dev_uninit() in the future.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Olivier Matz <olivier.matz@6wind.com>
    Signed-off-by: Ivan Delalande <colona@arista.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- net/8021q/vlan_netlink.c
+++ net/8021q/vlan_netlink.c
@@ -189,10 +189,11 @@ static int vlan_newlink(struct net *src_net, struct net_device *dev,
 		return -EINVAL;
 
 	err = vlan_changelink(dev, tb, data, extack);
-	if (!err)
+	if (err)
+		return err;
 		err = register_vlan_dev(dev, extack);
 	if (err)
-		vlan_dev_uninit(dev);
+		vlan_dev_free_egress_priority(dev);
 	return err;
 }
 
