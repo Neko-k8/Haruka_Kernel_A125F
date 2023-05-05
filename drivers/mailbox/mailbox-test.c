4f8b445b482b0886dfe8e64118cd9c6d08c38770
Dan Carpenter <dan.carpenter@linaro.org>
2023-05-05 12:22:09 +0300

    UPSTREAM: mailbox: mailbox-test: fix a locking issue in mbox_test_message_write()
    [ Upstream commit 8fe72b76db79d694858e872370df49676bc3be8c ]
    
    There was a bug where this code forgot to unlock the tdev->mutex if the
    kzalloc() failed.  Fix this issue, by moving the allocation outside the
    lock.
    
    Bug: 275340532
    Fixes: 2d1e952a2b8e ("mailbox: mailbox-test: Fix potential double-free in mbox_test_message_write()")
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>
    Reviewed-by: Lee Jones <lee@kernel.org>
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 7d233f93594f0d9afe44e9409131a8d6ad4f593c)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I7a4a1bf06abbb2092aceb72610e3f894b2bfbf0f

--- drivers/mailbox/mailbox-test.c
+++ drivers/mailbox/mailbox-test.c
@@ -117,10 +117,8 @@ static ssize_t mbox_test_message_write(struct file *filp,
 		return -EINVAL;
 	}
 
-	mutex_lock(&tdev->mutex);
-
-	tdev->message = kzalloc(MBOX_MAX_MSG_LEN, GFP_KERNEL);
-	if (!tdev->message)
+	message = kzalloc(MBOX_MAX_MSG_LEN, GFP_KERNEL);
+	if (!message)
 		return -ENOMEM;
 
 	mutex_lock(&tdev->mutex);
